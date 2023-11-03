1a95014dfdd3d252d1dc85dfa2986065c8b4c899
Alex <17780186+TheLexoPlexx@users.noreply.github.com>
2023-11-03 19:05:13 +0100

    Ditched Leptos and setup Vite+React

--- assistent/src-tauri/src/main.rs
+++ assistent/src-tauri/src/main.rs
@@ -2,6 +2,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    #[cfg(desktop)]
-    cereal_assistant_lib::run();
+  tauri::Builder::default()
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
