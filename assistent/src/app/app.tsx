ade2b83df828e47718f044d7ae4d2b8d4ff43631
Alex <17780186+TheLexoPlexx@users.noreply.github.com>
2023-11-07 09:51:41 +0100

    Tested the barcode-scanner-plugin, not working

--- assistent/src/app/app.tsx
+++ assistent/src/app/app.tsx
@@ -1,29 +1,22 @@
-import { useState } from 'react'
 import './app.module.scss'
+import { Format, scan } from "@tauri-apps/plugin-barcode-scanner";
 
 export function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-        </a>
-        <a href="https://react.dev" target="_blank">
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={(ev) => {
+        try {
+          scan({
+            windowed: true,
+            formats: [Format.QRCode],
+            cameraDirection: "back",
+          })
+        } catch (e) {
+          console.error(e)
+        }
+      }}>
+        Scan
+      </button>
     </>
   )
 }
--- poem-openapi/src/prisma_openapi.rs
+++ poem-openapi/src/prisma_openapi.rs
@@ -5,7 +5,7 @@
 // ------------------------------------------------------
 
 use chrono::NaiveDateTime;
-use poem::{error::InternalServerError, web::Data, Result};
+use poem::{web::Data, Result, error::InternalServerError};
 use poem_openapi::{payload::Json, Object, OpenApi};
 use sqlx::PgPool;
 
@@ -55,59 +55,50 @@ pub struct Factory {
     name: String,
 }
 
+
 pub struct Api;
 
 #[OpenApi]
 impl Api {
-    #[oai(path = "/system", method = "get")]
-    async fn get_system(&self, pool: Data<&PgPool>) -> Result<Json<Vec<System>>> {
-        let get = sqlx::query_as!(System, "SELECT * FROM \"System\"")
-            .fetch_all(pool.0)
-            .await
-            .expect("Failed to get System");
-        Ok(Json(get))
-    }
 
-    #[oai(path = "/system", method = "post")]
-    async fn post_system(&self, pool: Data<&PgPool>, input: Json<System>) -> Result<Json<String>> {
-        match sqlx::query("INSERT INTO \"System\" (id, label, order_number) values ($1, $2, $3)")
-            .bind(&input.id)
-            .bind(&input.label)
-            .bind(&input.order_number)
-            .execute(pool.0)
-            .await
-        {
-            Ok(x) => {
-                println!("Inserted System");
-                dbg!(x);
-                Ok(Json("ok".to_string()))
-            }
-            Err(e) => {
-                println!("Failed to insert System: {}", e);
-                Err(InternalServerError(e))
-            }
-        }
-    }
+#[oai(path = "/system", method = "get")]
+async fn get_system(&self, pool: Data<&PgPool>) -> Result<Json<Vec<System>>> {
+  let get = sqlx::query_as!(System, "SELECT * FROM \"System\"").fetch_all(pool.0).await.expect("Failed to get System");
+  Ok(Json(get))
+}
 
-    #[oai(path = "/serialnumberpart", method = "get")]
-    async fn get_serialnumberpart(
-        &self,
-        pool: Data<&PgPool>,
-    ) -> Result<Json<Vec<Serialnumberpart>>> {
-        let get = sqlx::query_as!(Serialnumberpart, "SELECT * FROM \"Serialnumberpart\"")
-            .fetch_all(pool.0)
-            .await
-            .expect("Failed to get Serialnumberpart");
-        Ok(Json(get))
+#[oai(path = "/system", method = "post")]
+async fn post_system(
+    &self,
+    pool: Data<&PgPool>,
+    input: Json<System>,
+) -> Result<Json<String>> {
+  match sqlx::query("INSERT INTO \"System\" (id, label, order_number) values ($1, $2, $3)").bind(&input.id).bind(&input.label).bind(&input.order_number).execute(pool.0).await {
+  Ok(x) => {
+      println!("Inserted System");
+      dbg!(x);
+      Ok(Json("ok".to_string()))
     }
+    Err(e) => {
+      println!("Failed to insert System: {}", e);
+      Err(InternalServerError(e))
+    }
+  }
+}
 
-    #[oai(path = "/serialnumberpart", method = "post")]
-    async fn post_serialnumberpart(
-        &self,
-        pool: Data<&PgPool>,
-        input: Json<Serialnumberpart>,
-    ) -> Result<Json<String>> {
-        match sqlx::query("INSERT INTO \"Serialnumberpart\" (serial, part_id, system_id, parent_id, manufacturing_date, shipping_date, installation_date) values ($1, $2, $3, $4, $5, $6, $7)").bind(&input.serial).bind(&input.edv_nummer).bind(&input.system_id).bind(&input.parent_id).bind(&input.manufacturing_date).bind(&input.shipping_date).bind(&input.installation_date).execute(pool.0).await {
+#[oai(path = "/serialnumberpart", method = "get")]
+async fn get_serialnumberpart(&self, pool: Data<&PgPool>) -> Result<Json<Vec<Serialnumberpart>>> {
+  let get = sqlx::query_as!(Serialnumberpart, "SELECT * FROM \"Serialnumberpart\"").fetch_all(pool.0).await.expect("Failed to get Serialnumberpart");
+  Ok(Json(get))
+}
+
+#[oai(path = "/serialnumberpart", method = "post")]
+async fn post_serialnumberpart(
+    &self,
+    pool: Data<&PgPool>,
+    input: Json<Serialnumberpart>,
+) -> Result<Json<String>> {
+  match sqlx::query("INSERT INTO \"Serialnumberpart\" (serial, part_id, system_id, parent_id, manufacturing_date, shipping_date, installation_date) values ($1, $2, $3, $4, $5, $6, $7)").bind(&input.serial).bind(&input.edv_nummer).bind(&input.system_id).bind(&input.parent_id).bind(&input.manufacturing_date).bind(&input.shipping_date).bind(&input.installation_date).execute(pool.0).await {
   Ok(x) => {
       println!("Inserted Serialnumberpart");
       dbg!(x);
@@ -118,141 +109,105 @@ impl Api {
       Err(InternalServerError(e))
     }
   }
-    }
+}
 
-    #[oai(path = "/project", method = "get")]
-    async fn get_project(&self, pool: Data<&PgPool>) -> Result<Json<Vec<Project>>> {
-        let get = sqlx::query_as!(Project, "SELECT * FROM \"Project\"")
-            .fetch_all(pool.0)
-            .await
-            .expect("Failed to get Project");
-        Ok(Json(get))
-    }
+#[oai(path = "/project", method = "get")]
+async fn get_project(&self, pool: Data<&PgPool>) -> Result<Json<Vec<Project>>> {
+  let get = sqlx::query_as!(Project, "SELECT * FROM \"Project\"").fetch_all(pool.0).await.expect("Failed to get Project");
+  Ok(Json(get))
+}
 
-    #[oai(path = "/project", method = "post")]
-    async fn post_project(
-        &self,
-        pool: Data<&PgPool>,
-        input: Json<Project>,
-    ) -> Result<Json<String>> {
-        match sqlx::query(
-            "INSERT INTO \"Project\" (id, project_number, description) values ($1, $2, $3)",
-        )
-        .bind(&input.id)
-        .bind(&input.project_number)
-        .bind(&input.description)
-        .execute(pool.0)
-        .await
-        {
-            Ok(x) => {
-                println!("Inserted Project");
-                dbg!(x);
-                Ok(Json("ok".to_string()))
-            }
-            Err(e) => {
-                println!("Failed to insert Project: {}", e);
-                Err(InternalServerError(e))
-            }
-        }
+#[oai(path = "/project", method = "post")]
+async fn post_project(
+    &self,
+    pool: Data<&PgPool>,
+    input: Json<Project>,
+) -> Result<Json<String>> {
+  match sqlx::query("INSERT INTO \"Project\" (id, project_number, description) values ($1, $2, $3)").bind(&input.id).bind(&input.project_number).bind(&input.description).execute(pool.0).await {
+  Ok(x) => {
+      println!("Inserted Project");
+      dbg!(x);
+      Ok(Json("ok".to_string()))
     }
+    Err(e) => {
+      println!("Failed to insert Project: {}", e);
+      Err(InternalServerError(e))
+    }
+  }
+}
 
-    #[oai(path = "/order", method = "get")]
-    async fn get_order(&self, pool: Data<&PgPool>) -> Result<Json<Vec<Order>>> {
-        let get = sqlx::query_as!(Order, "SELECT * FROM \"Order\"")
-            .fetch_all(pool.0)
-            .await
-            .expect("Failed to get Order");
-        Ok(Json(get))
-    }
+#[oai(path = "/order", method = "get")]
+async fn get_order(&self, pool: Data<&PgPool>) -> Result<Json<Vec<Order>>> {
+  let get = sqlx::query_as!(Order, "SELECT * FROM \"Order\"").fetch_all(pool.0).await.expect("Failed to get Order");
+  Ok(Json(get))
+}
 
-    #[oai(path = "/order", method = "post")]
-    async fn post_order(&self, pool: Data<&PgPool>, input: Json<Order>) -> Result<Json<String>> {
-        match sqlx::query("INSERT INTO \"Order\" (order_number, project_id) values ($1, $2)")
-            .bind(&input.order_number)
-            .bind(&input.project_id)
-            .execute(pool.0)
-            .await
-        {
-            Ok(x) => {
-                println!("Inserted Order");
-                dbg!(x);
-                Ok(Json("ok".to_string()))
-            }
-            Err(e) => {
-                println!("Failed to insert Order: {}", e);
-                Err(InternalServerError(e))
-            }
-        }
+#[oai(path = "/order", method = "post")]
+async fn post_order(
+    &self,
+    pool: Data<&PgPool>,
+    input: Json<Order>,
+) -> Result<Json<String>> {
+  match sqlx::query("INSERT INTO \"Order\" (order_number, project_id) values ($1, $2)").bind(&input.order_number).bind(&input.project_id).execute(pool.0).await {
+  Ok(x) => {
+      println!("Inserted Order");
+      dbg!(x);
+      Ok(Json("ok".to_string()))
     }
+    Err(e) => {
+      println!("Failed to insert Order: {}", e);
+      Err(InternalServerError(e))
+    }
+  }
+}
 
-    #[oai(path = "/finalcustomer", method = "get")]
-    async fn get_finalcustomer(&self, pool: Data<&PgPool>) -> Result<Json<Vec<FinalCustomer>>> {
-        let get = sqlx::query_as!(FinalCustomer, "SELECT * FROM \"FinalCustomer\"")
-            .fetch_all(pool.0)
-            .await
-            .expect("Failed to get FinalCustomer");
-        Ok(Json(get))
-    }
+#[oai(path = "/finalcustomer", method = "get")]
+async fn get_finalcustomer(&self, pool: Data<&PgPool>) -> Result<Json<Vec<FinalCustomer>>> {
+  let get = sqlx::query_as!(FinalCustomer, "SELECT * FROM \"FinalCustomer\"").fetch_all(pool.0).await.expect("Failed to get FinalCustomer");
+  Ok(Json(get))
+}
 
-    #[oai(path = "/finalcustomer", method = "post")]
-    async fn post_finalcustomer(
-        &self,
-        pool: Data<&PgPool>,
-        input: Json<FinalCustomer>,
-    ) -> Result<Json<String>> {
-        match sqlx::query(
-            "INSERT INTO \"FinalCustomer\" (id, sap_id, name, alias) values ($1, $2, $3, $4)",
-        )
-        .bind(&input.id)
-        .bind(&input.sap_id)
-        .bind(&input.name)
-        .bind(&input.alias)
-        .execute(pool.0)
-        .await
-        {
-            Ok(x) => {
-                println!("Inserted FinalCustomer");
-                dbg!(x);
-                Ok(Json("ok".to_string()))
-            }
-            Err(e) => {
-                println!("Failed to insert FinalCustomer: {}", e);
-                Err(InternalServerError(e))
-            }
-        }
+#[oai(path = "/finalcustomer", method = "post")]
+async fn post_finalcustomer(
+    &self,
+    pool: Data<&PgPool>,
+    input: Json<FinalCustomer>,
+) -> Result<Json<String>> {
+  match sqlx::query("INSERT INTO \"FinalCustomer\" (id, sap_id, name, alias) values ($1, $2, $3, $4)").bind(&input.id).bind(&input.sap_id).bind(&input.name).bind(&input.alias).execute(pool.0).await {
+  Ok(x) => {
+      println!("Inserted FinalCustomer");
+      dbg!(x);
+      Ok(Json("ok".to_string()))
     }
+    Err(e) => {
+      println!("Failed to insert FinalCustomer: {}", e);
+      Err(InternalServerError(e))
+    }
+  }
+}
 
-    #[oai(path = "/factory", method = "get")]
-    async fn get_factory(&self, pool: Data<&PgPool>) -> Result<Json<Vec<Factory>>> {
-        let get = sqlx::query_as!(Factory, "SELECT * FROM \"Factory\"")
-            .fetch_all(pool.0)
-            .await
-            .expect("Failed to get Factory");
-        Ok(Json(get))
-    }
+#[oai(path = "/factory", method = "get")]
+async fn get_factory(&self, pool: Data<&PgPool>) -> Result<Json<Vec<Factory>>> {
+  let get = sqlx::query_as!(Factory, "SELECT * FROM \"Factory\"").fetch_all(pool.0).await.expect("Failed to get Factory");
+  Ok(Json(get))
+}
 
-    #[oai(path = "/factory", method = "post")]
-    async fn post_factory(
-        &self,
-        pool: Data<&PgPool>,
-        input: Json<Factory>,
-    ) -> Result<Json<String>> {
-        match sqlx::query("INSERT INTO \"Factory\" (id, customer_id, name) values ($1, $2, $3)")
-            .bind(&input.id)
-            .bind(&input.customer_id)
-            .bind(&input.name)
-            .execute(pool.0)
-            .await
-        {
-            Ok(x) => {
-                println!("Inserted Factory");
-                dbg!(x);
-                Ok(Json("ok".to_string()))
-            }
-            Err(e) => {
-                println!("Failed to insert Factory: {}", e);
-                Err(InternalServerError(e))
-            }
-        }
+#[oai(path = "/factory", method = "post")]
+async fn post_factory(
+    &self,
+    pool: Data<&PgPool>,
+    input: Json<Factory>,
+) -> Result<Json<String>> {
+  match sqlx::query("INSERT INTO \"Factory\" (id, customer_id, name) values ($1, $2, $3)").bind(&input.id).bind(&input.customer_id).bind(&input.name).execute(pool.0).await {
+  Ok(x) => {
+      println!("Inserted Factory");
+      dbg!(x);
+      Ok(Json("ok".to_string()))
     }
+    Err(e) => {
+      println!("Failed to insert Factory: {}", e);
+      Err(InternalServerError(e))
+    }
+  }
+}
 }
